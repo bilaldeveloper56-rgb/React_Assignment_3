a6d877329758e76e984312e27769b1be33bf6369
Muhammad Bilal Khan <bilaldeveloper56@gmail.com>
2026-03-18 06:50:31 +0500

    apply box shadow

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Card = ({image, title, description }) => {
   return (
-     <div className='w-[20%] bg-white rounded flex flex-col gap-2 px-3 py-4 text-center'>
+     <div className='w-[20%] bg-white rounded flex flex-col gap-2 px-3 py-4 text-center hover:shadow-2xl'>
     <img className=' m-auto rounded' src={image} alt="" />
     <h1 className='font-bold'>{title}</h1>
     <hr />
     <p>{description}</p>
-    <button className='border bg-blue-700 text-white rounded py-1'>Read more</button>
+    <button className='border bg-blue-700 text-white rounded py-1 hover:bg-blue-900 hover:cursor-pointer'>Read more</button>
   </div> 
   )
 }
